5ecc43778e35856dd664855010e8d0c389956e12
phmiranda <pehhagah.1607@gmail.com>
2020-09-16 09:23:31 -0300

    [add] - finalização da aula 05 do curso de introdução a javascript.

--- src/app.v5.js
+++ src/app.v5.js
@@ -1,17 +1,31 @@
 /* Aula 02 - Atribuição de Variáveis */
 
+// a CONST é uma variável que não muda após usa atribuição.
+// const nome = "Pedro";
+// const sobrenome = "Henrique";
+// const nomeCompleto;
+// const idade = 27;
+// const contador = 0;
+
 // a LET é uma variável que pode ser alterada durante os blocos de código.
 let nome = "Pedro";
+let sobrenome = "Henrique";
+let idade = 27;
+let contador = 0;
 
-// a CONST é uma variável que não muda após usa atribuição.
-const sobrenome = "Henrique";
-const idade = 27;
+// exemplo utilizando a declaração com LET (não é indicado trabalhar dessa forma,pois é uma prática ruim e difícil de debugar).
+// nomeCompleto = nome + sobrenome;
 
-// utilizando a interpolação.
+// atribui novos valores as variáveis.
+nomeCompleto = nome + sobrenome;
+contador = contador + 1;
+idade = idade + 1;
+
+// utilizando a interpolação e declarando o valor das variáveis usando CONST.
 console.log(nome, sobrenome);
 console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos.`);
 
-// exemplo utilizando a declaração com LET (não é indicado trabalhar dessa forma,pois é uma prática ruim e difícil de debugar).
-nome = nome + sobrenome;
-console.log(nome);
-
+// novas exibições
+console.log(nomeCompleto);
+console.log(contador);
+console.log(idade);
